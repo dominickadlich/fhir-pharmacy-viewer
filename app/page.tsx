68a02c04563498a060b74456a2b16213214ede33
Dominick Adlich <139933162+dominickadlich@users.noreply.github.com>
2026-03-10 22:32:43 -0500

    Updated env variables on Vercel, metadata and changed main button color

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 export default function Home() {
     return (
         <main className="flex min-h-screen items-center justify-center bg-slate-900">
-            <Link href="/launch" className="px-6 py-3 rounded-full bg-cyan-600 text-white font-semibold hover:bg-cyan-500">
+            <Link href="/launch" className="px-6 py-3 rounded-full bg-violet-600 text-white font-semibold hover:bg-violet-500">
                 FHIRMACY VIEWER
             </Link>
         </main>
